32268fb39926ebb6729a09e489b308e5653bda3a
Alice <alice@ferretbeans.dev>
2025-09-06 20:26:47 +0800

    so like im pawsome >w<

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use warp::*;
+use warp::{filters::fs::file, *};
+use json::*;
+use std::{ptr::null, *};
 
 #[tokio::main]
 async fn main() {
-    let site = warp::path::end()
-        .and(warp::get())
-        .map(move || warp::reply::html(include_str!("guh.html")));
+    check_files();
+
+    let site = warp::fs::dir("./webpages");
 
     /*let api_upload = warp::path!("api" / "v1" / "upload")
         .and(warp::post())
         .and(//i need to have this allow file uploads)
         .and_then(file_upload); */
-    
 
     let route = site;
         //.or(dir);
@@ -19,4 +20,20 @@ async fn main() {
     warp::serve(route)
         .run(([0, 0, 0, 0], 3000))
         .await;
+}
+
+async fn file_upload() {
+
+}
+
+//Create the folder / check every time it is run for satefy c:
+fn check_files() -> std::io::Result<()> {
+    Ok(if std::fs::File::open("./datafiles/data.json").is_err() {
+    std::fs::DirBuilder::new().create("./datafiles");
+    assert!(std::fs::metadata("./datafiles").unwrap().is_dir());
+    std::fs::File::create("./datafiles/data.json")?;
+    println!("Created data");
+    } else {
+        println!("Loaded config and data");
+    })
 }
